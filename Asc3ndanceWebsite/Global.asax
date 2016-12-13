4d0e6e675242313eb5b5e9bba0153f8a4865ad91
JayLenahan <lenahanjay@gmail.com>
2016-12-13 16:21:23 -0600

    Initial Commit

---
+++ Asc3ndanceWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Asc3ndanceWebsite.MvcApplication" Language="C#" %>
---
+++ Asc3ndanceWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Asc3ndanceWebsite.Startup))]
+namespace Asc3ndanceWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
